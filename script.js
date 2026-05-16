dc06ac714ee3a06068de7d5ca8e596d22b7b9a29
mansurovshe-create <mansurov.she@icloud.com>
2026-05-17 00:14:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const MAX_TOKEN = "f9LHodD0cOLkSXTTDrDpKBdiksN0uV_fuY36iakrJrmDAEZwDxiW5BqRrWXqhsgaFzoQlDxu5dSxHqrwRuuQ";
-const MAX_CHAT_ID = "-74821219658404";
-
 document.getElementById("telegramForm").addEventListener("submit", async function(e){
 
 e.preventDefault();
@@ -11,27 +8,29 @@ const route = document.getElementById("route").value;
 const cargo = document.getElementById("cargo").value;
 
 try{
-await fetch(`https://platform-api.max.ru/messages?chat_id=${MAX_CHAT_ID}`, {
+
+await fetch("https://noisy-breeze-f037.mansurov-she.workers.dev", {
 method: "POST",
 headers: {
-"Authorization": MAX_TOKEN,
 "Content-Type": "application/json"
 },
 body: JSON.stringify({
-text: `🚛 Новая заявка
-
-👤 Имя: ${name}
-📞 Телефон: ${phone}
-📍 Маршрут: ${route}
-📦 Груз: ${cargo}`
+name,
+phone,
+route,
+cargo
 })
 });
 
 alert("Заявка отправлена");
+
 document.getElementById("telegramForm").reset();
 
 }catch(error){
+
+console.error(error);
 alert("Ошибка отправки");
+
 }
 
 });
